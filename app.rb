35e5064a143516bd4f707964cede082c63ac39f9
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-26 09:46:04 +0100

    Add App to Start the Cookbook Router

---
+++ app.rb
@@ -0,0 +1,10 @@
+# Initialize the CSV file
+csv_file = File.join(__dir__, "recipes.csv")
+# Create cookbook w/ csv
+cookbook = Cookbook.new(csv_file)
+# Create controller w/ cookbook
+controller = Controller.new(cookbook)
+
+# Create router w/ controller
+router = Router.new(controller)
+router.run
